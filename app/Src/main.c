399ea9338cdb56a9f130167a5eaa34dfa9f39139
slemanz <william_sleman@hotmail.com>
2025-10-19 08:12:31 -0300

    app(feat): begin to use gpio driver

--- app/Src/main.c
+++ app/Src/main.c
@@ -1,21 +1,19 @@
 #include <stdint.h>
+#include "ra4m1.h"
 
-#define MMIO32(addr) (*(volatile uint32_t *)(addr))
-#define MMIO8(addr)  (*(volatile uint8_t *)(addr))
+#include "driver_gpio.h"
+
 
 int main(void)
 {
     // config pin
-    MMIO8(0x40040D03) |= (1 << 6);
+    GPIO_PfsweControl(ENABLE);
     MMIO32(0x40040020) |= (1 << 11);
-    MMIO8(0x40040D03) &= ~(1 << 6);
+    GPIO_PfsweControl(DISABLE);
 
     while(1)
     {
-        MMIO32(0x40040020) ^= (1U << 27);
-        for(uint32_t i = 0; i < 20000; i++)
-        {
-            __asm("NOP");
-        }
+        GPIO_ToggleOutputPin(PORT1, GPIO_PIN_NO_11);
+        for(uint32_t i = 0; i < 30000; i++) __asm("NOP");
     }
 }
