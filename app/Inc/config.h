849c4aef4a7db04043a5b0995fd7cdfc3dff6d8a
slemanz <william_sleman@hotmail.com>
2025-11-09 06:13:30 -0300

    app(feat): add config file

---
+++ app/Inc/config.h
@@ -0,0 +1,8 @@
+#ifndef INC_CONFIG_H_
+#define INC_CONFIG_H_
+
+#include <stdint.h>
+
+void config_drivers(void);
+
+#endif /* INC_CONFIG_H_ */
---
+++ app/Src/config.c
@@ -0,0 +1,9 @@
+#include "config.h"
+
+// drivers
+#include "driver_gpio.h"
+
+void config_drivers(void)
+{
+
+}
