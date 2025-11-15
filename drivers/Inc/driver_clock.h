6ec69f28c209b795c8e79ef78e98836c13233cb0
slemanz <william_sleman@hotmail.com>
2025-11-15 15:46:10 -0300

    drivers(feat): add clock include file

---
+++ drivers/Inc/driver_clock.h
@@ -0,0 +1,9 @@
+#ifndef INC_DRIVER_CLOCK_H_
+#define INC_DRIVER_CLOCK_H_
+
+#include "ra4m1.h"
+
+void clock_init(void);
+uint32_t clock_system_get(void);
+
+#endif /* INC_DRIVER_CLOCK_H_ */
--- drivers/Inc/ra4m1.h
+++ drivers/Inc/ra4m1.h
@@ -7,9 +7,12 @@
 
 #define __vo volatile
 #define MMIO32(addr) (*(volatile uint32_t *)(addr))
+#define MMIO16(addr)  (*(volatile uint16_t *)(addr))
 #define MMIO8(addr)  (*(volatile uint8_t *)(addr))
 
 /************************** BASE ADDRESSES **************************/
+
+
 
 #define PWPR_BASEADDR               0x40040D03UL
 #define PFS_BASEADDR                0x40040800UL
@@ -21,6 +24,25 @@
 #define SCS_BASE            				(0xE000E000UL)
 #define SYSTICK_BASEADDR					(SCS_BASE + 0x0010UL)
 
+/************************** REG ADDRESSES ***************************/
+
+// Write Protection
+#define REG_PRCR_ADDRESS            0x4001E3FEUL
+
+// Clock
+#define REG_SCKDIVCR_ADDRESS        0x4001E020UL
+#define REG_SCKSCR_ADDRESS          0x4001E026UL
+#define REG_MEMWAIT_ADDRESS         0x4001E031UL
+#define REG_HOCOCR_ADDRESS          0x4001E036UL
+
+/*********************** OFFSET DEFINITIONS *************************/
+
+#define SCKDIVCR_FCK_OFFSET         28
+#define SCKDIVCR_ICK_OFFSET         24
+#define SCKDIVCR_PCKA_OFFSET        12
+#define SCKDIVCR_PCKB_OFFSET        8
+#define SCKDIVCR_PCKC_OFFSET        4
+#define SCKDIVCR_PCKD_OFFSET        0
 
 /*********************** STRUCTS DEFINITIONS ************************/
 
@@ -55,5 +77,7 @@ typedef struct
 
 #define INTERRUPT_ENABLE()          do{__asm volatile ("MOV R0, #0x0"); __asm volatile("MSR PRIMASK, R0");}while(0)
 #define INTERRUPT_DISABLE()         do{__asm volatile ("MOV R0, #0x1"); __asm volatile("MSR PRIMASK, R0");}while(0)
+
+#define PRCR_KEY_CODE               0xA500
 
 #endif /* INC_RA4M1_H_ */
